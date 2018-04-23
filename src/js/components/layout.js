e49156ae85a5d11dcb5cf6254beab073d850619c
kevinhsu <kevinhsu@synology.com>
2018-04-24 01:36:00 +0800

    Add button example

--- src/js/components/layout.js
+++ src/js/components/layout.js
@@ -1,7 +1,8 @@
 import React from "react";
-import Button from 'material-ui-next/Button';
-import MuiThemeProvider from 'material-ui-next/styles/MuiThemeProvider';
+import Button from 'material-ui/Button';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import styles from './layout.css';
+import FloatingActionButtons from './button';
 
 export default class Layout extends React.Component {
 	constructor() {
@@ -16,6 +17,7 @@ export default class Layout extends React.Component {
 				<Button variant="raised" color="primary">
 					Hello World
 				</Button>
+				<FloatingActionButtons />
 			</MuiThemeProvider>
 		);
 	}
